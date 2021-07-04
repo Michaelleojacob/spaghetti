79bc4a61899e36c114b0d21ad37a234b12ceedf9
Michael <michaelleojacob@gmail.com>
2021-07-03 22:25:13 -0700

    need to figure out how to move the span a bit more to the right. I want to believe there is a way I can just move it to the end of the li

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const ulpeople = document.querySelector("#people");
 
 const render = function (template, node){
     if (!node) return;
-    node.innerHTMl = template;
+    node.textContent = template;
 }
 
-addPersonButton.addEventListener("keydown", function(e){
+addPersonButton.addEventListener("click", function(e){
     console.log(e);
     const personName = personinfo.value;
     people.push(personName);
@@ -17,5 +17,25 @@ addPersonButton.addEventListener("keydown", function(e){
         people: people,
     }
     console.log(data);
-    // ulpeople.appendChild(render("li", data))
+    myperson = document.createElement('li');
+    myperson.setAttribute("id", personName);
+    render(personName, myperson);
+    ulpeople.appendChild(myperson);
+    delbtn = document.createElement("SPAN");
+    myperson.appendChild(delbtn);
+    delbtn.textContent = "X";
+    personinfo.value = "";
 });
+
+footer = document.querySelector(".footer");
+myul = document.createElement("ul");
+myul.textContent = "hello";
+myil = document.createElement("li");
+myil.textContent = "kenobi";
+myspan = document.createElement("SPAN");
+myspan.textContent = "X";
+footer.appendChild(myul);
+myul.appendChild(myil);
+myil.appendChild(myspan);
+
+
