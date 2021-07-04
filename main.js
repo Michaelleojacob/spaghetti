47c74f5ca9128021e3b7a6dbe737d22421686ce1
Michael <michaelleojacob@gmail.com>
2021-07-04 09:08:46 -0700

    starting up the module version of the sphaghetti code. So far script = spaghetti, and main.js =  modular version

--- main.js
+++ main.js
@@ -5,13 +5,18 @@
             this.cacheDom();
         },
         cacheDom: function(){
-            this.el = document.querySelector("#people");
+            this.el = document.body.querySelector("#peopleModule");
+            this.ul = this.el.querySelector("#people");
             this.btn = this.el.querySelector("#addPerson");
-            this.pinfo = document.querySelector(".personinfo");
-            console.log(this.el);
+            this.pinfo = this.el.querySelector(".personinfo");
+            // console.log(this.el);
+            // console.log(this.ul);
             // console.log(this.btn);
             // console.log(this.pinfo);
-        }
+        },
+        render: function(){
+            
+        },
     }
     people.init();
 })()
