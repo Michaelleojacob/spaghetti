f19fd41efc788c42c9b08892ef85431716e32a3a
Michael <michaelleojacob@gmail.com>
2021-07-04 08:11:09 -0700

    span now works the way I want it to, figured out how to move the span towards the edge of the li. can delete the person from the page. Currently working on how to also delete from the people array

--- script.js
+++ script.js
@@ -10,32 +10,42 @@ const render = function (template, node){
 }
 
 addPersonButton.addEventListener("click", function(e){
-    console.log(e);
     const personName = personinfo.value;
     people.push(personName);
     const data = {
         people: people,
     }
-    console.log(data);
     myperson = document.createElement('li');
-    myperson.setAttribute("id", personName);
+    myperson.classList.add(personName);
     render(personName, myperson);
     ulpeople.appendChild(myperson);
     delbtn = document.createElement("SPAN");
+    delbtn.classList.add(personName);
+    delbtn.classList.add("del");
     myperson.appendChild(delbtn);
     delbtn.textContent = "X";
     personinfo.value = "";
 });
 
-footer = document.querySelector(".footer");
-myul = document.createElement("ul");
-myul.textContent = "hello";
-myil = document.createElement("li");
-myil.textContent = "kenobi";
-myspan = document.createElement("SPAN");
-myspan.textContent = "X";
-footer.appendChild(myul);
-myul.appendChild(myil);
-myil.appendChild(myspan);
+document.addEventListener("click", function(e){
+    // console.log(e);
+    if(e.target.className.includes("del")){
+        console.log(people);
+        ulpeople.removeChild(myperson);
+        delete people[myperson];
+        console.log(people);
+    }
+})
+
+// footer = document.querySelector(".footer");
+// myul = document.createElement("ul");
+// myul.textContent = "hello";
+// myil = document.createElement("li");
+// myil.textContent = "kenobi";
+// myspan = document.createElement("SPAN");
+// myspan.textContent = "X";
+// footer.appendChild(myul);
+// myul.appendChild(myil);
+// myil.appendChild(myspan);
 
 
