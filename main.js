61b55a208d529aa2f10c3fe9a076a0d8af347f4a
Michael <michaelleojacob@gmail.com>
2021-07-04 11:54:34 -0700

    renderToPage is able to add new people based on user input properly. No more duplicates. It deletes all dom elements on the page before then establishing each element from the array to the page.

--- main.js
+++ main.js
@@ -12,29 +12,27 @@
             this.ul = this.el.querySelector("#people");
             this.btn = this.el.querySelector("#addPerson");
             this.pinfo = this.el.querySelector(".personinfo");
-            // console.log(this.el);
-            // console.log(this.ul);
-            // console.log(this.btn);
-            // console.log(this.pinfo);
         },
         pushToArr: function(){
             const personinfo = this.pinfo;
             this.btn.addEventListener("click", function(e){
                 if(personinfo.value !== ""){people.people.push(personinfo.value)};
-                // console.log(people.people);
                 personinfo.value = '';
                 people.renderToPage(people.people);
             });
         },
         renderToPage: function(arr){
+            //remove all prior people
+            console.log(people.ul);
+            while(this.ul.lastElementChild) {
+                this.ul.removeChild(this.ul.lastElementChild);
+            }
             //take each element from people.people arr and display them on the page
-            console.log(arr);
             arr.forEach(element => {
                 //display element to page as li
                 const personli = document.createElement("li");
                 personli.textContent = element;
                 this.ul.appendChild(personli);
-
             });
         },
     }
