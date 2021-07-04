61683c6ee9003fbf1d2303f05b4a5b763c43ac31
Michael <michaelleojacob@gmail.com>
2021-07-04 08:36:00 -0700

    cleaned up the code slightly, removed a function I wasn't reusing, moved some code, and got the delete feature unbugged. It wasn't deleting multiple items, had an error. Now it deletes correctly from the page and the array

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-const people = [];
+let people = [];
 const template = document.querySelector("#people-template");
 const addPersonButton = document.querySelector("#addPerson");
 const personinfo = document.querySelector(".personinfo");
 const ulpeople = document.querySelector("#people");
 
-const render = function (template, node){
-    if (!node) return;
-    node.textContent = template;
+
+
+function makeLiAndSpanDelBtn(pname){
+    myperson = document.createElement('li');
+    myperson.classList.add(pname);
+    myperson.textContent = pname;
+    ulpeople.appendChild(myperson);
+    delbtn = document.createElement("span");
+    delbtn.classList.add(pname, "del");
+    myperson.appendChild(delbtn);
+    delbtn.textContent = "X";
+    personinfo.value = "";
 }
 
 addPersonButton.addEventListener("click", function(e){
     const personName = personinfo.value;
     people.push(personName);
-    const data = {
-        people: people,
-    }
-    myperson = document.createElement('li');
-    myperson.classList.add(personName);
-    render(personName, myperson);
-    ulpeople.appendChild(myperson);
-    delbtn = document.createElement("SPAN");
-    delbtn.classList.add(personName);
-    delbtn.classList.add("del");
-    myperson.appendChild(delbtn);
-    delbtn.textContent = "X";
-    personinfo.value = "";
+    makeLiAndSpanDelBtn(personName);
 });
 
 document.addEventListener("click", function(e){
-    // console.log(e);
     if(e.target.className.includes("del")){
-        console.log(people);
-        ulpeople.removeChild(myperson);
-        delete people[myperson];
-        console.log(people);
+        const name = e.target.classList[0];
+        people = people.filter(item => item !== name);
+        document.querySelector(`.${name}`).remove();
     }
 })
-
-// footer = document.querySelector(".footer");
-// myul = document.createElement("ul");
-// myul.textContent = "hello";
-// myil = document.createElement("li");
-// myil.textContent = "kenobi";
-// myspan = document.createElement("SPAN");
-// myspan.textContent = "X";
-// footer.appendChild(myul);
-// myul.appendChild(myil);
-// myil.appendChild(myspan);
-
-
